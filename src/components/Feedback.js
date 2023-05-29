b2e4762bc76e354928c58d26222fdc030b6c7da6
David Lin <lindavid1998@gmail.com>
2023-05-29 11:47:11 -0700

    Update feedback message to indicate when game is complete

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,39 +1,93 @@
 import React from 'react';
-import styled from 'styled-components/macro';
+import styled, { keyframes } from 'styled-components/macro';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 
+const fadeInAnimation = keyframes`
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
 const Message = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 200px;
-  font-size: 1.5rem;
-  padding: 2rem;
-  transition: 0.5s opacity ease-in-out;
-  color: ${(props) => (props.isPositive ? 'var(--success-color)' : 'red')};
-  opacity: ${(props) => (props.isVisible ? '1' : '0')};
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 200px;
+	font-size: 1.5rem;
+	padding: 0.5rem;
+	transition: 0.5s opacity ease-in-out;
+	color: ${(props) => (props.isPositive ? 'var(--success-color)' : 'red')};
+	opacity: ${(props) => (props.isVisible ? '1' : '0')};
+`;
+
+const Main = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 10px;
+`;
+
+const Buttons = styled.div`
+	display: ${(props) => (props.isGameComplete ? 'flex' : 'none')};
+	justify-content: center;
+	align-items: center;
+	gap: 13px;
+`;
+
+const Button = styled.button`
+	background: ${(props) => (props.$primary ? 'white' : 'black')};
+	color: ${(props) => (props.$primary ? 'black' : 'white')};
+	font-size: 1em;
+	padding: 0.4em 1em;
+	border: 2px solid white;
+	transition: all 0.5s ease;
+	animation: ${fadeInAnimation} 1.5s forwards;
+	&:hover {
+		transform: translateY(-5px);
+    cursor: pointer;
+	}
 `;
 
 const Feedback = () => {
-  const message = useSelector((state) => state.feedbackMsg);
-  const numOfAttempts = useSelector((state) => state.numOfAttempts);
-  const isPositive = message === 'Nice job!';
-  const [isVisible, setIsVisible] = useState(true);
+	const message = useSelector((state) => state.feedbackMsg);
+	const numOfAttempts = useSelector((state) => state.numOfAttempts);
+	const targets = useSelector((state) => state.targets);
+	const isGameComplete = targets.every((target) => target.isFound);
 
-  useEffect(() => {
+	const isPositive = isGameComplete || message?.includes('Nice job!') || false;
+
+	const [isVisible, setIsVisible] = useState(true);
+
+	useEffect(() => {
 		setIsVisible(true);
-		const timer = setTimeout(() => {
-			setIsVisible(false);
-		}, 2000);
+		let timer;
+		if (!isGameComplete) {
+			timer = setTimeout(() => {
+				setIsVisible(false);
+			}, 2000);
+		}
 		return () => clearTimeout(timer);
-	}, [message, numOfAttempts]);
+	}, [isGameComplete, numOfAttempts]);
 
-  return (
-    <Message isPositive={isPositive} isVisible={isVisible}>
-      {message}
-    </Message>
-  );
+	return (
+		<Main>
+			<Message isPositive={isPositive} isVisible={isVisible}>
+				{message}
+			</Message>
+			<Buttons isGameComplete={isGameComplete}>
+				<Button $primary>Submit time</Button>
+				<Button>Leaderboard</Button>
+			</Buttons>
+		</Main>
+	);
 };
 
 export default Feedback;
--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,49 +1,53 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import styled from 'styled-components/macro';
+import { useSelector, useDispatch } from 'react-redux';
+import { setFeedback } from '../redux/actions';
 
 const StyledDiv = styled.div`
-  font-size: 2.5rem;
-  font-weight: bold;
-  width: 160px;
+	font-size: 2.5rem;
+	font-weight: bold;
+	width: 160px;
 `;
 
 const convertSecondsToHMS = (timeInSeconds) => {
-  let hours = Math.floor(timeInSeconds / 3600);
-  let minutes = Math.floor((timeInSeconds - hours * 3600) / 60);
-  let seconds = timeInSeconds - hours * 3600 - minutes * 60;
-  let timeArray = [hours.toString(), minutes.toString(), seconds.toString()];
+	let hours = Math.floor(timeInSeconds / 3600);
+	let minutes = Math.floor((timeInSeconds - hours * 3600) / 60);
+	let seconds = timeInSeconds - hours * 3600 - minutes * 60;
+	let timeArray = [hours.toString(), minutes.toString(), seconds.toString()];
 
-  timeArray = timeArray.map((time) => {
-    if (time.length === 1) {
-      return '0' + time;
-    } else {
-      return time;
-    }
-  });
+	timeArray = timeArray.map((time) => {
+		if (time.length === 1) {
+			return '0' + time;
+		} else {
+			return time;
+		}
+	});
 
-  return timeArray.join(':');
+	return timeArray.join(':');
 };
 
 const Stopwatch = () => {
-  const [secondsElapsed, setSecondsElapsed] = useState(0);
-  const targets = useSelector((state) => state.targets);
+	const [secondsElapsed, setSecondsElapsed] = useState(0);
+	const targets = useSelector((state) => state.targets);
   const isGameComplete = targets.every((target) => target.isFound);
+  const dispatch = useDispatch();
 
-  useEffect(() => {
-    let intervalId;
-    if (!isGameComplete) {
+	useEffect(() => {
+		let intervalId;
+		if (isGameComplete) {
+			dispatch(setFeedback('Game complete'));
+    } else {
       intervalId = setInterval(() => {
-        setSecondsElapsed((prevSecondsElapsed) => prevSecondsElapsed + 1);
-      }, 1000);
+				setSecondsElapsed((prevSecondsElapsed) => prevSecondsElapsed + 1);
+			}, 1000);
     }
-    return () => clearInterval(intervalId);
-  }, [isGameComplete]);
+		return () => clearInterval(intervalId);
+	}, [isGameComplete]);
 
-  const displayedTime = convertSecondsToHMS(secondsElapsed);
+	const displayedTime = convertSecondsToHMS(secondsElapsed);
 
-  return <StyledDiv data-testid="stopwatch">{displayedTime}</StyledDiv>;
+	return <StyledDiv data-testid='stopwatch'>{displayedTime}</StyledDiv>;
 };
 
 export default Stopwatch;
